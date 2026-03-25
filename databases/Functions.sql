9e69e0f307e0fa1b737c2b039e9bd488b88a0aa2
camilosssss <141536887+camilosssss@users.noreply.github.com>
2026-03-24 21:10:53 -0500

    Update Functions.sql
    añadi funcion ver repartidor :)(

--- databases/Functions.sql
+++ databases/Functions.sql
@@ -10,3 +10,24 @@ BEGIN
 END //
 
 DELIMITER ;
+ DELIMITER //
+
+CREATE FUNCTION VerRepartidor(CodigoRepartidor INT)
+RETURNS VARCHAR(50)
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE v_nombre VARCHAR(50);
+
+    SELECT Nombres 
+      INTO v_nombre 
+      FROM Persona 
+     WHERE CodigoRepartidor = CodigoPersona
+     LIMIT 1;
+
+    -- Siempre debes devolver algo
+    RETURN COALESCE(v_nombre, 'REPARTIDOR NO ENCONTRADO');
+
+END //
+
+DELIMITER ;
