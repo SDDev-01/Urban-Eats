e41009fe8a6daac18b10c3736f40a4104018b5c2
SDDev-01 <nuevocubi+1@gmail.com>
2026-06-15 00:53:58 -0500

    agregue el logout

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegistroController;
+use App\Http\Controllers\LogoutController;
 
-//por ahora estaticas, si despues tomamos datos entonces ahi si usamos route:get
+//por ahora estaticas, si despues tomamos datos entonces ahi si usamos route:get o post
 
+//vistas estaticas
 Route::view('/','index');
 Route::view('/carrito','carrito');
 Route::view('/catalogo','catalogo');
 Route::view('/cliente','cliente');
-Route::view('/login','login');
 Route::view('/mapa','mapa');
 Route::view('/pago','pago');
 Route::view('/perfil','perfil');
 Route::view('/perfilRestaurante','perfilRestaurante');
 Route::view('/rastreo','rastreo');
-Route::view('/registro','registro');
 Route::view('/repartidor','repartidor');
 Route::view('/restaurante','restaurante');
 Route::view('/restauranteDetalle','restauranteDetalle');
@@ -31,8 +31,5 @@ Route::post('/login',[LoginController::class, 'iniciarSesion']);
 Route::get('/registro',[RegistroController::class, 'mostrarPagina']);
 Route::post('/registro',[RegistroController::class, 'Registrarse']);
 
-/* 
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
+//cerrar sesion
+Route::post('/logout',[LogoutController::class, 'logout']);
