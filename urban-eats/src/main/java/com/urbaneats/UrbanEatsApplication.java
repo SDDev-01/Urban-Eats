6bc95dfa5462d7d9b837a407fccb942eeb8c1277
SDDev-01 <nuevocubi+1@gmail.com>
2026-08-16 00:58:48 -0500

    mensaje de commit pq nadie lee esto

---
+++ urban-eats/src/main/java/com/urbaneats/UrbanEatsApplication.java
@@ -0,0 +1,13 @@
+package com.urbaneats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanEatsApplication.class, args);
+	}
+
+}
